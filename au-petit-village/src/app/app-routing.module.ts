5c5967907108170afcffe2a43ce03e03c49c8cdd
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2025-01-11 13:42:48 +0100

    Etape4_Configurer le routing et les pages

--- au-petit-village/src/app/app-routing.module.ts
+++ au-petit-village/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { Error404Component } from './error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: Error404Component }    // Route pour la page "error 404"
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
