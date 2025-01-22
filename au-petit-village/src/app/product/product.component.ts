56c636bb3598db172257296fc2acf8eb8de4b6d1
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2025-01-22 20:35:25 +0100

    Etape11c_Implémentation de Product. Version avec lien entre Home et les fiches de chaque produit.

--- au-petit-village/src/app/product/product.component.ts
+++ au-petit-village/src/app/product/product.component.ts
@@ -1,46 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: false,
-
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-  product = {
-    name: 'Figurine du Gaulois motorisé',
-    price: 39.99,
-    imageUrl: '/images/products/figurine-gaulois-moderne-scotter.jpg',
-    legende: 'Figurine du soldat Gaulois moderne (sur son vespa...).',
-    details: {
-      description: [
-        { idParagraphe: 1, contentParagraphe: 'Premier paragraphe de la description longue du produit.' },
-        { idParagraphe: 2, contentParagraphe: 'Second paragraphe de la description longue du produit.' },
-        { idParagraphe: 3, contentParagraphe: 'Dernier paragraphe de la description longue du produit.' },
-      ],
-      features: [
-        { label: 'matière', content: 'procelaine' },
-        { label: 'dimensions', content: '20x10x25cm (Lxlxh)' },
-        { label: 'masse', content: '0,8kg' },
-        { label: 'colorisation', content: 'peinture acrylique' }
-      ],
-      delivery: [
-        { label: 'méthode', content: 'livreur' },
-        { label: 'délais', content: '3-5 jours ouvrés' },
-        { label: 'coût', content: 'forfait 5€' },
-      ],
-      stock: 10,
-    },
-  };
-
+  product: any;
   liked: boolean = false;
-
   expandedParagraphs: number[] = [1];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.product = JSON.parse(params['product']);
+    });
+  }
 
   toggleLike(): void {
     this.liked = !this.liked;
@@ -54,7 +32,7 @@ export class ProductComponent implements OnInit {
   }
 
   expandAllParagraphs(): void {
-    this.expandedParagraphs = this.product.details.description.map(p => p.idParagraphe);
+    this.expandedParagraphs = this.product.details.description.map((p: { idParagraphe: number }) => p.idParagraphe);
   }
 
   collapseParagraph(paragraphId: number): void {
